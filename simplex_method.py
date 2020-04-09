da72cf9c33727385e410ec0707a81a953f0faaa2
Ahirui Otsu <otsuahirui@gmail.com>
2020-04-10 08:26:23 +0900

    Remove unnecessary import

--- simplex_method.py
+++ simplex_method.py
@@ -23,7 +23,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from sympy import Matrix, pprint
+from sympy import Matrix
 
 status = ('Optimal', 'Feasible', 'Unbounded', 'Infeasible', 'Continue')
 OPTIMAL = 0
